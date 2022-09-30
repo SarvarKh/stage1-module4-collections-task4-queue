390b8c61a7c999a1cbf6647a83a066b6696c89bb
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-09-30 12:11:59 +0500

    Solve 2. ArrayDeque creator

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,12 +1,31 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
+    public static void main(String[] args) {
+        ArrayDequeCreator deque = new ArrayDequeCreator();
+        Queue<Integer> firstQ = new LinkedList<>(List.of(1, 3, 5, 4, 7, 9));
+        Queue<Integer> secondQ = new LinkedList<>(List.of(4, 2, 8, 5, 8, 3));
+        deque.createArrayDeque(firstQ, secondQ);
+    }
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
-        ArrayDeque<Integer> result = new ArrayDeque<>();
-        return result;
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.add(firstQueue.remove());
+        deque.add(firstQueue.remove());
+        deque.add(secondQueue.remove());
+        deque.add(secondQueue.remove());
+
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            firstQueue.add(deque.removeLast());
+            deque.add(firstQueue.remove());
+            deque.add(firstQueue.remove());
+
+            secondQueue.add(deque.removeLast());
+            deque.add(secondQueue.remove());
+            deque.add(secondQueue.remove());
+        }
+
+        return deque;
     }
 }
